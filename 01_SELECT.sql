b24983a9ef90443e5a21f07822c6e378a58b3d1a
peb7544 <peb7544@naver.com>
2024-02-29 17:37:38 +0900

    sql

--- 01_SELECT.sql
+++ 01_SELECT.sql
@@ -266,3 +266,48 @@ AND SALARY >= 2700000;
  * 7. AND (논리연산자)
  * 8. OR (논리연산자)
  * */
+
+-------------------------------------------------------------------------------------
+
+/* ORDER BY
+ * 
+ * - SELECT 문의 조회 결과(RESULT SET)를 정렬할 때 사용하는 구문
+ * 
+ * ** SELECT문 해석 시 가장 마지막에 해석된다!!!
+ * 
+ * 3. SELECT절
+ * 1. FROM절
+ * 2. WHERE절
+ * 4. ORDER BY 컬럼명 | 별칭 | 컬럼 순서 [ASC | DESC] [NULL FIRST | LAST]
+ *                            (오름차순:기본값) (내림차순)
+ * */
+
+-- EMPLOYEE 테이블에서 급여 오름차순으로
+-- 사번, 이름, 급여 조회
+SELECT EMP_ID, EMP_NAME, SALARY  
+FROM EMPLOYEE
+ORDER BY SALARY;
+
+-- 급여 200만 이상인 사원의
+-- 사번, 이름, 급여 조회
+-- 단, 급여 내림 차순으로 조회
+SELECT 
+	EMP_ID, EMP_NAME, SALARY
+FROM EMPLOYEE
+WHERE SALARY  >= 2000000
+ORDER BY 3 DESC;
+
+-- 입사일 순서대로 이름, 입사일 조회(별칭 사용)
+SELECT 
+	EMP_NAME 이름, HIRE_DATE 입사일
+FROM EMPLOYEE
+ORDER BY 입사일;
+
+/* 정렬 중첩 : 대분류 정렬 후 소분류 정렬 */
+
+-- 부서코드 오름차순 정렬 후 급여 내림차순 정렬
+
+SELECT 
+	EMP_NAME, DEPT_CODE, SALARY 
+FROM EMPLOYEE
+ORDER BY DEPT_CODE, SALARY ASC;
