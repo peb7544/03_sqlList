ba14d49a58705c8077860eb248ff3050d12191f6
peb7544 <peb7544@naver.com>
2024-03-08 16:30:34 +0900

    sql

--- 07_SUBQUERY_실습.sql
+++ 07_SUBQUERY_실습.sql
@@ -6,9 +6,25 @@ SELECT
 	,EMP_NAME 
 	,PHONE 
 	,TO_CHAR(HIRE_DATE, 'YY/MM/DD') HIRE_DATE
+	,DEPT_TITLE
 FROM EMPLOYEE
+LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)
 WHERE DEPT_CODE = (SELECT DEPT_CODE FROM EMPLOYEE WHERE EMP_NAME = '전지연')
 AND EMP_NAME != '전지연';
+
+----------------------------------------------------------------------------
+
+SELECT 
+	 EMP_ID
+	,EMP_NAME 
+	,PHONE 
+	,HIRE_DATE
+	,DEPT_TITLE
+FROM EMPLOYEE
+LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)
+WHERE DEPT_CODE = (SELECT DEPT_CODE FROM EMPLOYEE WHERE EMP_NAME = '전지연');
+AND EMP_NAME != '전지연';
+
 
 -- 2. 고용일이 2000년도 이후인 사원들 중 급여가 가장 높은 사원의
 --    사번, 사원명, 전화번호, 급여, 직급명을 조회하시오.
@@ -36,6 +52,21 @@ SELECT
 FROM EMPLOYEE
 JOIN JOB USING(JOB_CODE)
 LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)
+WHERE (DEPT_CODE, JOB_CODE) = (SELECT DEPT_CODE, JOB_CODE FROM EMPLOYEE WHERE EMP_NAME = '노옹철')
+AND EMP_NAME != '노옹철';
+
+--------------------------------------------------------------------------------------------------
+
+SELECT 
+	 EMP_ID
+	,EMP_NAME 
+	,DEPT_CODE 
+	,JOB_CODE 
+	,DEPT_TITLE
+	,JOB_NAME
+FROM EMPLOYEE
+JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)
+NATURAL JOIN JOB
 WHERE (DEPT_CODE, JOB_CODE) = (SELECT DEPT_CODE, JOB_CODE FROM EMPLOYEE WHERE EMP_NAME = '노옹철')
 AND EMP_NAME != '노옹철';
 
@@ -80,9 +111,15 @@ SELECT
 FROM EMPLOYEE 
 JOIN JOB USING(JOB_CODE)
 LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)
-WHERE ENT_YN = 'N'
-AND HIRE_DATE IN (SELECT MIN(HIRE_DATE) FROM EMPLOYEE WHERE ENT_YN = 'N' GROUP BY DEPT_CODE)
+WHERE HIRE_DATE IN (SELECT MIN(HIRE_DATE) FROM EMPLOYEE WHERE ENT_YN = 'N' GROUP BY DEPT_CODE)
 ORDER BY HIRE_DATE;
+-- 부서별로 그룹을 묶을 때 퇴사한 직원을 서브쿼리에서 제외해야함.
+-- 왜? 부서별로 가장 빠른 입사자 구했을 때 'D8' 부서는 이태림임(이태림 퇴사자)
+--> 문제점 : 부서별로 가장 빠른 입사자 구해놓고, 메인쿼리에서 퇴사자 제외해버리면
+-- DB 부서는 퇴사자인 이태림이 가장 빠른 입사자이기 때문에
+-- 전체 부서 중 DB 부서가 아예 제외되어 버림
+--> 부서별 가장 빠른 입사자구할 때(서브쿼리) 퇴사한 직원을 뺀 상태에서 그룹으로 묶으면
+-- DB 부서의 가장 빠른 입사자는 이테림 제외 후 전형돈이 됨.
 
 -- 7. 직급별 나이가 가장 어린 직원의
 --    사번, 이름, 직급명, 나이, 보너스 포함 연봉을 조회하고
@@ -93,7 +130,7 @@ SELECT
 	,EMP_NAME 
 	,JOB_NAME
 	,나이
-	,'\' || TO_CHAR(보너스포함연봉, 'FM999,999,999') 보너스포함연봉
+	,'￦' || TO_CHAR(보너스포함연봉, 'FM999,999,999') 보너스포함연봉
 FROM (
 SELECT 
 	 EMP_ID 
@@ -114,8 +151,24 @@ ORDER BY 나이 DESC;
 
 
 
+--------------------------------------------------------------------------------------------------
 
+-- 서브쿼리
 
+SELECT MAX(EMP_NO) FROM EMPLOYEE
+GROUP BY JOB_CODE;
+
+-- 메인쿼리
+SELECT 
+	 EMP_ID 
+	,EMP_NAME 
+	,JOB_NAME
+	,FLOOR(MONTHS_BETWEEN(SYSDATE, TO_DATE(SUBSTR(EMP_NO,1,6), 'RRMMDD' )) / 12) 나이
+	,TO_CHAR(SALARY * (1 + NVL(BONUS,0)) * 12, 'L999,999,999') "보너스 포함 연봉"
+FROM EMPLOYEE
+NATURAL JOIN JOB
+WHERE EMP_NO IN (SELECT MAX(EMP_NO) FROM EMPLOYEE GROUP BY JOB_CODE)
+ORDER BY 나이 DESC;
 
 
 
