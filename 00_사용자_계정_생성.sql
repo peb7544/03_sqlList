5635966d6298b7b6cde38d82382d563627ee2950
peb7544 <peb7544@naver.com>
2024-05-16 12:19:15 +0900

    sql

--- 00_사용자_계정_생성.sql
+++ 00_사용자_계정_생성.sql
@@ -31,6 +31,22 @@ GRANT RESOURCE, CONNECT TO exam_peb;
 -- 객체가 생성될 수 있는 공간 할당량 지정
 ALTER USER exam_peb DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 
+DROP USER kh_peb CASCADE;
+
+ALTER SESSION SET "_oracle_script"=TRUE;
+
+
+
+-- 11G 버전 이전의 문접을 사용 가능하도록 함
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 사용자 계정 생성
+CREATE USER kh_peb IDENTIFIED BY kh1234;
+
+-- 사용자 권한 부여
+GRANT RESOURCE, CONNECT TO kh_peb;
+
+-- 객체가 생성될 수 있는 공간 할당량 지정
+ALTER USER kh_peb DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
 DROP USER exam_PEB CASCADE;
-
-
